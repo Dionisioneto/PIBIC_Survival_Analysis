2716c7e59d518c2fad43e7e48345f05eed08e55a
Dionisio Neto <dionisioneto899@gmail.com>
2023-04-24 18:58:00 -0300

    inicio do processo de geracao de fracao de cura

--- esboco_fracaodecura.R
+++ esboco_fracaodecura.R
@@ -42,7 +42,7 @@ x1 = rnorm(n = tamanho.amostral, mean = 0, sd = 1)
 x2 = rbinom(n = tamanho.amostral, size = 1, prob = 0.5)
 matriz = cbind(rep(1, tamanho.amostral), x1, x2)
 
-betas = c(1.2, 1.2, 0)
+betas = c(1.2, 0.5, 0)
 
 z = betas[1] + betas[2]*x1 + betas[3]*x2
   
@@ -52,8 +52,8 @@ Y = rbinom(tamanho.amostral,1,prob.z)
 
 ## Passo 2: Proporcao de censuras
 
-c1 = 0.46
-c2 = 0.32
+c1 = runif(n = tamanho.amostral, min = 0.6, max = 1)
+c2 = runif(n = tamanho.amostral, min = 0.42, max = 1)
 
 A = rexp(n = tamanho.amostral, rate = 1)
 
@@ -95,7 +95,6 @@ loglik.int.fc = function(par, time.l, time.r,
   n.covars = dim(x.matrix)[2] ## numero de covariaveis
   betas = par[(b + 2):(b + 1 + n.covars)]
   
-  
   ## calculo da populacao curada
   
   pi = exp(x.matriz %*% betas)/(1 + exp(x.matriz %*% betas))
@@ -114,13 +113,12 @@ loglik.int.fc = function(par, time.l, time.r,
   
   sr = s0.tr^exp((x.matrix[delta==1,] %*% betas))
   
+  p1 = delta*(1-pi)*(sl - sr)
   
   ## contribuicao da censura
   s0.tl = PPE(time = time.l[delta==0], cuts = grid, levels = hazards, alpha = exp, type = "survival")
   sl = s0.tl^exp((x.matrix[delta==0,] %*% betas))
   
-  
-  p1 = delta*(1-pi)*(sl - sr)
   p2 = (1-delta)*(pi + ((1-pi)*sl))
   
   log.vero = log(p1 + p2)
@@ -129,14 +127,52 @@ loglik.int.fc = function(par, time.l, time.r,
   
 }
 
+## funcao de geracao de dados com censura intervalar e fracao de cura
+sim_bch <- function(N, theta = c(1, 0.5, 0),
+                    lambda = 1, A = 5, B = 15, prob = 0.5){
+  intercept <- 1
+  xi1 <- stats::rbinom(N, 1, prob)
+  xi2 <- stats::rnorm(N)
+  X <- data.frame(intercept, xi1, xi2)
+  lambda_pois <- as.numeric(exp(theta %*% t(X)))
+  N_L <- stats::rpois(N, lambda_pois)
+  a <- stats::rexp(N)
+  C <- cbind(A,a * B)
+  C <- C[,1] * (C[,1] <= C[,2]) + C[,2] * (C[,1] > C[,2])
+  T <- c(1:N) * NA
+  for (i in 1:N) {
+    T[i] <- ifelse(N_L[i] > 0, min(stats::rexp(N_L[i],
+                                               lambda)), Inf)
+  }
+  delta <- ifelse(T <= C,1,0)
+  Z <- ifelse(T <= C, T, C)
+  L <- R <- Z * NA
+  for (i in 1:N){
+    if (delta[i] == 0){
+      L[i] <- Z[i]
+      R[i] <- Inf
+    }
+    else{
+      L[i] <- 0
+      add  <- stats::runif(1, 0.1, 0.5)
+      R[i] <- add
+      check <- (L[i] <= Z[i] & Z[i] < R[i])
+      while(!check){
+        L[i] <- L[i] + add
+        add <- stats::runif(1, 0.1, 0.5)
+        R[i] <- R[i] + add
+        check <- (L[i] <= Z[i] & Z[i] < R[i])
+      }
+    }
+  }
+  dados <- data.frame(Z, L, R, delta, xi1, xi2, N_latent = N_L)
+  return(dados)
+}
 
-
-
-
-
-
-
-
+sim_bch(N = 100,
+        theta = c(1,0.5,-0.6),
+        lambda = 1,
+        A = 5, B = 10, prob = 0.5)
 
 
 
