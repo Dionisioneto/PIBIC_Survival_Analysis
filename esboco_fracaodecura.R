374c56244e5f3959851de74f46609954dbc211fc
Dionisio Neto <dionisioneto899@gmail.com>
2023-04-28 19:49:46 -0300

    geracao de dados sob fracao de cura

--- esboco_fracaodecura.R
+++ esboco_fracaodecura.R
@@ -18,12 +18,14 @@
 ## com covariaveis
 ## ------
 
-tamanho.amostral=1000
+N =50
 
-survival.time = rep(0, tamanho.amostral)
-delta = rep(0, tamanho.amostral)
+survival.time = rep(NA, N)
+L = rep(NA, N)
+R = rep(NA, N)
+delta = rep(NA, N)
 
-## Colunas
+# # Colunas
 # B: v.a Bernoulli para indicar curado e nao curado
 # L: limite inferior do intervalo observado
 # R: limite superior do intervalo observado
@@ -36,192 +38,276 @@ delta = rep(0, tamanho.amostral)
 ## Passo 1: geracao de uma variavel aleatoria bernoulli, sob o modelo
 ## logistico com covariaveis
 
-# set.seed(10)
-# 
-# x1 = rnorm(n = tamanho.amostral, mean = 0, sd = 1)
-# x2 = rbinom(n = tamanho.amostral, size = 1, prob = 0.5)
-# matriz = cbind(rep(1, tamanho.amostral), x1, x2)
-# 
-# betas = c(1.2, 0.5, 0)
-# 
-# z = betas[1] + betas[2]*x1 + betas[3]*x2
-#   
-# prob.z = 1/(1+exp(-z))
-# Y = rbinom(tamanho.amostral,1,prob.z)
-# 
-# 
-# ## Passo 2: Proporcao de censuras
-# 
-# c1 = runif(n = tamanho.amostral, min = 0.6, max = 1)
-# c2 = runif(n = tamanho.amostral, min = 0.42, max = 1)
-# 
-# A = rexp(n = tamanho.amostral, rate = 1)
-# 
-# C = pmin(rep(c1,tamanho.amostral), c2 * A)
-# 
-# ## Passo 3: Se Y = 0, entao T = C e adota-se delta = 0.
-# 
-# survival.time[Y == 0] = C[Y==0]
-# delta[Y == 0] = rep(0, length(Y[Y==0]))
-# 
-# 
-# ## Passo 4: Se Y = 1, gera-se T por meio de uma distribuicao de risco condicional
-# ## Geracao de dados para o Modelo Exponencial por Partes por Potencia com efeito de covariaveis
-# 
-# betas = c(1.2, 0, 0.5)
-# t = exp(matriz%*%betas)
-# 
-# ifelse(t <= C,1,0)
-# 
-# delta[Y == 1] = rep(0, length(Y[Y==1]))
-# 
-# 
-# 
-# 
-# 
-# ## ------
-# ## Funcao de verosimilhanca para censura intervalar e fracao de cura, 
-# ## com covariaveis
-# ## ------
-# 
-# 
-# loglik.int.fc = function(par, time.l, time.r,
-#                          grid, delta, x.matriz){
-#   
-#   b = length(grid) + 1 ## numero de intervalos
-#   hazards = par[1:b] ## taxas de falha para os b intervalos
-#   exp = par[b + 1] ## parametro de potencia
-#   
-#   n.covars = dim(x.matrix)[2] ## numero de covariaveis
-#   betas = par[(b + 2):(b + 1 + n.covars)]
-#   
-#   ## calculo da populacao curada
-#   
-#   pi = exp(x.matriz %*% betas)/(1 + exp(x.matriz %*% betas))
-#   
-#   ## calculo da populacao nao-curada
-#   
-#   ## informacoes exponencial por partes Potencia (PPE) para esquerda
-#   s0.tl = PPE(time = time.l[delta==1], cuts = grid, levels = hazards, alpha = exp, type = "survival")
-#   
-#   ## informacoes exponencial por partes Potencia (PPE) para direita
-#   s0.tr = PPE(time = time.r[delta==1], cuts = grid, levels = hazards, alpha = exp, type = "survival")
-#   
-#   ## contribuicao das covariaveis
-#   
-#   sl = s0.tl^exp((x.matrix[delta==1,] %*% betas))
-#   
-#   sr = s0.tr^exp((x.matrix[delta==1,] %*% betas))
-#   
-#   p1 = delta*(1-pi)*(sl - sr)
-#   
-#   ## contribuicao da censura
-#   s0.tl = PPE(time = time.l[delta==0], cuts = grid, levels = hazards, alpha = exp, type = "survival")
-#   sl = s0.tl^exp((x.matrix[delta==0,] %*% betas))
-#   
-#   p2 = (1-delta)*(pi + ((1-pi)*sl))
-#   
-#   log.vero = log(p1 + p2)
-#   
-#   return(-1*log.vero)
-#   
-# }
-# 
-# ## funcao de geracao de dados com censura intervalar e fracao de cura
-# sim_bch <- function(N, theta = c(1, 0.5, 0),
-#                     lambda = 1, A = 5, B = 15, prob = 0.5){
-#   
-#   ## geracao de dados fracao de cura
-#   intercept <- 1
-#   xi1 <- stats::rbinom(N, 1, prob)
-#   xi2 <- stats::rnorm(N)
-#   X <- data.frame(intercept, xi1, xi2)
-#   
-#   lambda_pois <- as.numeric(exp(theta %*% t(X)))
-#   N_L <- stats::rpois(N, lambda_pois)
-#   a <- stats::rexp(N)
-#   C <- cbind(A,a * B)
-#   C <- C[,1] * (C[,1] <= C[,2]) + C[,2] * (C[,1] > C[,2])
-#   T <- c(1:N) * NA
-#   
-#   for (i in 1:N) {
-#     T[i] <- ifelse(N_L[i] > 0, min(stats::rexp(N_L[i],
-#                                                lambda)), Inf)
-#   }
-#   
-#   delta <- ifelse(T <= C,1,0)
-#   Z <- ifelse(T <= C, T, C)
-#   L <- R <- Z * NA
-#   for (i in 1:N){
-#     if (delta[i] == 0){
-#       L[i] <- Z[i]
-#       R[i] <- Inf
-#     }
-#     else{
-#       L[i] <- 0
-#       add  <- stats::runif(1, 0.1, 0.5)
-#       R[i] <- add
-#       check <- (L[i] <= Z[i] & Z[i] < R[i])
-#       while(!check){
-#         L[i] <- L[i] + add
-#         add <- stats::runif(1, 0.1, 0.5)
-#         R[i] <- R[i] + add
-#         check <- (L[i] <= Z[i] & Z[i] < R[i])
-#       }
-#     }
-#   }
-#   dados <- data.frame(Z, L, R, delta, xi1, xi2, N_latent = N_L)
-#   return(dados)
-# }
-# 
-# 
-# sim.IC.FC.data = function(n, betas = c(1, 0.5, 0), lambda, 
-#                           A, B, prob.bernoulli){
-#   
-#   ## intercepto
-#   intercepto = 1
-#   
-#   ## dados das preditoras
-#   xi1 = stats::rbinom(n, 1, prob = prob.bernoulli)
-#   xi2 = stats::rnorm(n, mean = 0, sd = 1)
-#   
-#   z = cbind(intercepto, xi1, xi2)
-#   
-#   risco.cond = as.numeric(exp(betas %*% z))
-#   
-#   a = runif(n = n, min = 0, max = 1)
-#   C = cbind(A, a * B)
-#   C <- C[,1] * (C[,1] <= C[,2]) + C[,2] * (C[,1] > C[,2])
-# 
-#   t = c(1:N) * NA 
-#   
-#   ## funcao que gere os tempos com fracao de cura
-#   for (i in 1:N) {
-#     T[i] <- ifelse(N_L[i] > 0, min(stats::rexp(N_L[i],
-#                                                lambda)), Inf)
-#   }
-#   
-# }
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# dados.fc = sim_bch(N = 100,
-#         theta = c(1,0.5,-0.6),
-#         lambda = 1,
-#         A = 5, B = 10, prob = 0.5)
-# 
-# head(dados.fc)
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+
+
+cbind(survival.time, L , R, delta, Y)
+
+dados.cens = survival::Surv(survival.time,delta)
+km_fit <- survival::survfit(dados.cens ~ 1)
+
+plot(km_fit)
+
+sim.cure.icen = function(N, prob.ber, betas.cure, betas.risk,
+                         c1, c2){
+  
+  ## Armazenameto de dados
+  
+  survival.time = rep(NA, N)
+  L = rep(NA, N)
+  R = rep(NA, N)
+  delta = rep(NA, N)
+  
+  ## geracao dos dados
+  x2 = rnorm(n = N, mean = 0, sd = 1)
+  x1 = rbinom(n = N, size = 1, prob = prob.ber)
+  matriz = cbind(rep(1, N), x1, x2)
+  
+  b = betas.cure
+  
+  z = b[1] + b[2]*x1 + b[3]*x2
+  
+  prob.z = 1/(1+exp(-z))
+  
+  Y = rbinom(N,1,prob.z)
+  
+  ## Proporcao de censuras
+  
+  A = rexp(n = N, rate = 1)
+  
+  C = cbind(c1,c2*A)
+  C = pmin(C[,1], C[,2])
+  
+  ##------
+  for(i in 1:N){
+    if(Y[i] == 0){
+      survival.time[i] = C[i]
+      delta[i] = 0
+    }
+    
+    else{
+      survival.time[i] = exp(matriz[i,-1] %*% betas.risk)
+      delta[i] = ifelse(survival.time[i] <= C[i], 1, 0)
+    }
+    
+    if(delta[i] == 0){
+      L[i] = C[i]
+      R[i] = Inf
+    }
+    
+    else{
+      L[i] = 0
+      Qj = runif(1, min = 0.1, max = 0.5)
+      R[i] = Qj
+      check = (L[i] <= survival.time[i] & survival.time[i] < R[i])
+      
+      while(!check){
+        L[i] = L[i] + Qj
+        Qj = runif(1, min = 0.1, max = 0.5)
+        R[i] = R[i] + Qj
+        check = (L[i] <= survival.time[i] & survival.time[i] < R[i])
+      }
+    }
+  }
+  data = data.frame(time = survival.time, L = L, R = R, 
+                    delta = delta, risk = Y, X1 = matriz[,2], X2 = matriz[,3])
+  return(data)
+}
+
+dados = sim.cure.icen(N = 100, prob.ber = 0.5, betas.cure = c(1.2,0.5,1.2),
+              betas.risk = c(0.5,0.3), c1 = 12, c2 = 17)
+
+#library(survival)
+
+dados.cens = Surv(dados$time, dados$delta)
+km = survfit(dados.cens~1)
+
+plot(km)
+
+## ------
+## Funcao de verosimilhanca para censura intervalar e fracao de cura,
+## com covariaveis
+## ------
+
+
+loglik.int.fc = function(par, time.l, time.r,
+                         grid, delta, x.matriz){
+
+  b = length(grid) + 1 ## numero de intervalos
+  hazards = par[1:b] ## taxas de falha para os b intervalos
+  exp = par[b + 1] ## parametro de potencia
+
+  n.covars = dim(x.matrix)[2] ## numero de covariaveis
+  betas = par[(b + 2):(b + 1 + n.covars)]
+
+  ## calculo da populacao curada
+
+  pi = exp(x.matriz %*% betas)/(1 + exp(x.matriz %*% betas))
+
+  ## calculo da populacao nao-curada
+
+  ## informacoes exponencial por partes Potencia (PPE) para esquerda
+  s0.tl = PPE(time = time.l[delta==1], cuts = grid, levels = hazards, alpha = exp, type = "survival")
+
+  ## informacoes exponencial por partes Potencia (PPE) para direita
+  s0.tr = PPE(time = time.r[delta==1], cuts = grid, levels = hazards, alpha = exp, type = "survival")
+
+  ## contribuicao das covariaveis
+
+  sl = s0.tl^exp((x.matrix[delta==1,] %*% betas))
+
+  sr = s0.tr^exp((x.matrix[delta==1,] %*% betas))
+
+  p1 = delta*(1-pi)*(sl - sr)
+
+  ## contribuicao da censura
+  s0.tl = PPE(time = time.l[delta==0], cuts = grid, levels = hazards, alpha = exp, type = "survival")
+  sl = s0.tl^exp((x.matrix[delta==0,] %*% betas))
+
+  p2 = (1-delta)*(pi + ((1-pi)*sl))
+
+  log.vero = log(p1 + p2)
+
+  return(-1*log.vero)
+
+}
+
+## funcao de geracao de dados com censura intervalar e fracao de cura
+sim_bch <- function(N, theta = c(1, 0.5, 0),
+                    lambda = 1, A = 5, B = 15, prob = 0.5){
+
+  ## geracao de dados fracao de cura
+  intercept <- 1
+  xi1 <- stats::rbinom(N, 1, prob)
+  xi2 <- stats::rnorm(N)
+  X <- data.frame(intercept, xi1, xi2)
+
+  lambda_pois <- as.numeric(exp(theta %*% t(X)))
+  N_L <- stats::rpois(N, lambda_pois)
+  a <- stats::rexp(N)
+  C <- cbind(A,a * B)
+  C <- C[,1] * (C[,1] <= C[,2]) + C[,2] * (C[,1] > C[,2])
+  T <- c(1:N) * NA
+
+  for (i in 1:N) {
+    T[i] <- ifelse(N_L[i] > 0, min(stats::rexp(N_L[i],
+                                               lambda)), Inf)
+  }
+
+  delta <- ifelse(T <= C,1,0)
+  Z <- ifelse(T <= C, T, C)
+  L <- R <- Z * NA
+  for (i in 1:N){
+    if (delta[i] == 0){
+      L[i] <- Z[i]
+      R[i] <- Inf
+    }
+    else{
+      L[i] <- 0
+      add  <- stats::runif(1, 0.1, 0.5)
+      R[i] <- add
+      check <- (L[i] <= Z[i] & Z[i] < R[i])
+      while(!check){
+        L[i] <- L[i] + add
+        add <- stats::runif(1, 0.1, 0.5)
+        R[i] <- R[i] + add
+        check <- (L[i] <= Z[i] & Z[i] < R[i])
+      }
+    }
+  }
+  dados <- data.frame(Z, L, R, delta, xi1, xi2, N_latent = N_L)
+  return(dados)
+}
+
+## especificacoes amostrais
+N = 100
+prob.ber = 0.5
+
+betas = c(1.2, 0.5, 1.5)
+A = 12
+B = 17
+lambda = 1
+
+## geracao de dados fracao de cura
+intercept <- 1
+xi1 <- stats::rbinom(N, 1, prob.ber)
+xi2 <- stats::rnorm(N)
+X <- data.frame(intercept, xi1, xi2)
+
+lambda_pois <- as.numeric(exp(betas %*% t(X)))
+
+N_L <- stats::rpois(N, lambda_pois)
+
+a <- stats::rexp(N)
+C <- cbind(A,a * B)
+C <- pmin(C[,1], C[,2])
+
+
+tempo <- c(1:N) * NA
+
+for(unidade in 1:N){
+  tempo[unidade] <- ifelse(N_L[unidade] >= 0, min(stats::rexp(N_L[unidade],lambda)), Inf)
+}
+
+delta = ifelse(tempo <= C, 1,0)
+
+Z.tempo = ifelse(tempo <= C, tempo, C)
+
+L = R = Z.tempo*NA
+
+for(unidade in 1:N){
+  if (delta[unidade] == 0){
+    L[unidade] = Z.tempo[unidade]
+    R[unidade] = Inf
+  }
+  else{
+    L[unidade] = 0
+    Qj = stats::runif(1, 0.1, 0.5)
+    R[unidade] = Qj
+    verifica = (L[unidade] <= Z.tempo[unidade] & Z.tempo[unidade] < R[unidade])
+  }
+  while(!check){
+    L[unidade] = L[unidade] + Qj
+    Qj = stats::runif(1, 0.1, 0.5)
+    R[unidade] = R[unidade] + Qj
+    verifica = (L[unidade] <= Z.tempo[unidade] & Z.tempo[unidade] < R[unidade])
+  }
+}
+
+dados = data.frame()
+
+for (i in 1:N){
+  if (delta[i] == 0){
+    L[i] <- Z[i]
+    R[i] <- Inf
+  }
+  else{
+    L[i] <- 0
+    add  <- stats::runif(1, 0.1, 0.5)
+    R[i] <- add
+    check <- (L[i] <= Z[i] & Z[i] < R[i])
+    while(!check){
+      L[i] <- L[i] + add
+      add <- stats::runif(1, 0.1, 0.5)
+      R[i] <- R[i] + add
+      check <- (L[i] <= Z[i] & Z[i] < R[i])
+    }
+  }
+}
+dados <- data.frame(Z, L, R, delta, xi1, xi2, N_latent = N_L)
+return(dados)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
